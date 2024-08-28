90f4c6a18e4671b88af95938880e0d2481c2db98
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-08-28 16:53:06 +0200

    add validation logic

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
+    const pattern = /^pet_[a-zA-Z0-9]+$/;
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+        if (pattern.test(input)) {
+            result = 'Valid Syntax' ;
+        } else {
+            result = 'Invalid Syntax' ;
+        }
 
             document.getElementById('result').innerText = result;
 }
