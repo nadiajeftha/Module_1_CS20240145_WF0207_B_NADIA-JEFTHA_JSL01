499cfbe142a0353c00e381d7477e24b015266531
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-08-28 23:18:31 +0200

    remove last document.getElementById...

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
     const pattern = /^pet_[a-zA-Z0-9]+$/;
-    let result = ''; // Placeholder for validation result
+    let resultElement = document.getElementById('result'); 
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
         if (pattern.test(input)) {
-            result = 'Valid Syntax' ;
+            resultElement.innerText = '✅ Valid Syntax' ;
         } else {
-            result = 'Invalid Syntax' ;
+            resultElement.innerText = '❌ Invalid Syntax' ;
         }
-
-            document.getElementById('result').innerText = result;
 }
+
 
 
